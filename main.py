7dbb49ed27c03577dfe178b5f9af8e9a38175dc6
Juan Carlos de Jesus Hernandez Granados <54817307+juancarlosdjhg@users.noreply.github.com>
2025-04-09 16:22:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -58,15 +58,9 @@ def readParams(fichajes: List[str]):
     if not fichajes:
         return {"No hay fichajes"}
     
-    print("Hora servidor: ")
-    print(datetime.now())
-
     zona_horaria = pytz.timezone('Atlantic/Canary')
     ahora_dt = datetime.now(zona_horaria)
     
-    print("Hora local: ")
-    print(ahora_dt)
-
     ahora = ahora_dt.strftime("%H:%M")
     fichajes_times = [
     zona_horaria.localize(datetime.combine(ahora_dt.date(), datetime.strptime(f, "%H:%M").time()))
@@ -75,43 +69,43 @@ def readParams(fichajes: List[str]):
     
     tiempo_trabajado = timedelta()
     tiempo_descansos = timedelta()
-    
     for i in range(0, len(fichajes_times) - 1, 2):
         tiempo_trabajado += fichajes_times[i + 1] - fichajes_times[i]
         if i + 2 < len(fichajes_times):
             tiempo_descansos += fichajes_times[i + 2] - fichajes_times[i + 1]
 
-    # Establecer el tiempo base de trabajo como 7:30 (siempre fijo)
     tiempo_trabajo_base = timedelta(hours=7, minutes=30)
-
-    # Tiempo total que debe transcurrir desde la entrada hasta la salida considerando descansos mínimos de 30 minutos
     descanso_minimo = timedelta(minutes=30)
     tiempo_total_jornada = tiempo_trabajo_base + max(tiempo_descansos, descanso_minimo)
-
     tiempo_salida_estimada = timedelta(hours=fichajes_times[0].hour, minutes=fichajes_times[0].minute) + tiempo_total_jornada
-
-     # Calcular tiempo transcurrido hasta ahora
     tiempo_trabajado_ahora = timedelta()
+    
     for i in range(0, len(fichajes_times), 2):
         if i + 1 < len(fichajes_times):
             tiempo_trabajado_ahora += fichajes_times[i + 1] - fichajes_times[i]
         else:
             tiempo_trabajado_ahora += ahora_dt - fichajes_times[i]
 
+    tiempo_restante_ahora = tiempo_trabajo_base - tiempo_trabajado_ahora
+    if tiempo_restante_ahora < timedelta():
+        tiempo_restante_ahora = timedelta()
+
     horas, minutos, _ = convert_timedelta(tiempo_trabajado)
     hora_salida, minuto_salida, _ = convert_timedelta(tiempo_salida_estimada)
     hora_des, minuto_des, _ = convert_timedelta(tiempo_descansos)
     hora_ahora, minuto_ahora, _ = convert_timedelta(tiempo_trabajado_ahora)
+    hora_rest_ahora, minuto_rest_ahora, _ = convert_timedelta(tiempo_restante_ahora)
 
     if tiempo_trabajado > tiempo_trabajo_base:
         tiempo_sobra = tiempo_trabajado - tiempo_trabajo_base
         horas, minutos, _ = convert_timedelta(tiempo_sobra)
         return {
             "Hora de entrada": fichajes_times[0].time(),
+            "Tiempo usado en descansos": f"{formatTo2Digits(hora_des)}:{formatTo2Digits(minuto_des)}:00",
             "Tiempo trabajado hasta último fichaje": str(tiempo_trabajado).zfill(8),
+            "Tiempo restante desde último fichaje": "00:00:00",
             "Tiempo trabajado hasta ahora": f"{formatTo2Digits(hora_ahora)}:{formatTo2Digits(minuto_ahora)}:00",
-            "Tiempo descansos": f"{formatTo2Digits(hora_des)}:{formatTo2Digits(minuto_des)}:00",
-            "Tiempo restante": "00:00:00",
+            "Tiempo restante hasta ahora": f"{formatTo2Digits(hora_rest_ahora)}:{formatTo2Digits(minuto_rest_ahora)}:00",
             "Hora de salida estimada": f"{formatTo2Digits(hora_salida)}:{formatTo2Digits(minuto_salida)}:00",
             "Tiempo sobrante": f"{formatTo2Digits(horas)}:{formatTo2Digits(minutos)}:00"
         }
@@ -120,9 +114,10 @@ def readParams(fichajes: List[str]):
         hora_res, minuto_res, _ = convert_timedelta(tiempo_restante)
         return {
             "Hora de entrada": fichajes_times[0].time(),
+            "Tiempo usado en descansos": f"{formatTo2Digits(hora_des)}:{formatTo2Digits(minuto_des)}:00",
             "Tiempo trabajado hasta último fichaje": str(tiempo_trabajado).zfill(8),
+            "Tiempo restante hasta último fichaje": f"{formatTo2Digits(hora_res)}:{formatTo2Digits(minuto_res)}:00",
             "Tiempo trabajado hasta ahora": f"{formatTo2Digits(hora_ahora)}:{formatTo2Digits(minuto_ahora)}:00",
-            "Tiempo descansos": f"{formatTo2Digits(hora_des)}:{formatTo2Digits(minuto_des)}:00",
-            "Tiempo restante": f"{formatTo2Digits(hora_res)}:{formatTo2Digits(minuto_res)}:00",
+            "Tiempo restante hasta ahora": f"{formatTo2Digits(hora_rest_ahora)}:{formatTo2Digits(minuto_rest_ahora)}:00",
             "Hora de salida estimada": f"{formatTo2Digits(hora_salida)}:{formatTo2Digits(minuto_salida)}:00"
         }
